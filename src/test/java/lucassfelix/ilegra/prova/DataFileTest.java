9fce4687a3f55ff960ce022fc6c529f92fde3bf1
Lucas Félix <lsfelix321@gmail.com>
2020-12-22 16:12:40 -0300

    data file tests

--- src/test/java/lucassfelix/ilegra/prova/DataFileTest.java
+++ src/test/java/lucassfelix/ilegra/prova/DataFileTest.java
@@ -1,7 +1,11 @@
 package lucassfelix.ilegra.prova;
 
+import lucassfelix.ilegra.prova.dataBuilders.ItemBuilder;
+import lucassfelix.ilegra.prova.dataBuilders.SaleBuilder;
+import lucassfelix.ilegra.prova.dataBuilders.SalesmanBuilder;
 import lucassfelix.ilegra.prova.dataObjects.Business;
 import lucassfelix.ilegra.prova.dataObjects.DataFile;
+import lucassfelix.ilegra.prova.dataObjects.Sale;
 import lucassfelix.ilegra.prova.dataObjects.Salesman;
 import org.junit.Assert;
 import org.junit.Test;
@@ -53,6 +57,111 @@ public class DataFileTest {
         actualCount = mockDataFile.clientAmount();
 
         Assert.assertEquals(actualCount,mockBusinessList.size());
+    }
+
+    @Test
+    public void shouldReturnMostExpensiveSale()
+    {
+
+        List<Sale> mockSaleList = new ArrayList<>();
+        mockSaleList.add(
+                SaleBuilder.builder()
+                        .withSaleId(1)
+                        .withSalesmanName("Lucas")
+                        .withNewItem(
+                            ItemBuilder.builder()
+                                    .withItemId(1)
+                                    .withItemQuantity(1)
+                                    .withItemPrice(10)
+                                    .build()
+                ).build());
+
+        mockSaleList.add(
+                SaleBuilder.builder()
+                        .withSaleId(2)
+                        .withSalesmanName("Lucas")
+                        .withNewItem(
+                                ItemBuilder.builder()
+                                        .withItemId(1)
+                                        .withItemQuantity(2)
+                                        .withItemPrice(10)
+                                        .build()
+                        ).build());
+
+        mockSaleList.add(
+            SaleBuilder.builder()
+                    .withSaleId(3)
+                    .withSalesmanName("Lucas")
+                    .withNewItem(
+                            ItemBuilder.builder()
+                                    .withItemId(1)
+                                    .withItemQuantity(3)
+                                    .withItemPrice(10)
+                                    .build()
+                    ).build());
+
+        DataFile testDataFile = new DataFile("test",null,null,mockSaleList);
+
+        int actual = testDataFile.mostExpensiveSaleId();
+
+        Assert.assertEquals(3,actual);
+    }
+
+    @Test
+    public void shouldReturnWorstSalesman()
+    {
+        List<Salesman> mockSalesmanList = new ArrayList<>();
+
+        mockSalesmanList.add(SalesmanBuilder.builder()
+                .withName("Lucas").withCPF("00000000000").withSalary(1000.0).build());
+
+
+        mockSalesmanList.add(SalesmanBuilder.builder()
+                .withName("Marco").withCPF("00000000000").withSalary(1000.0).build());
+
+
+        List<Sale> mockSaleList = new ArrayList<>();
+        mockSaleList.add(
+                SaleBuilder.builder()
+                        .withSaleId(1)
+                        .withSalesmanName("Marco")
+                        .withNewItem(
+                                ItemBuilder.builder()
+                                        .withItemId(1)
+                                        .withItemQuantity(1)
+                                        .withItemPrice(10)
+                                        .build()
+                        ).build());
+
+        mockSaleList.add(
+                SaleBuilder.builder()
+                        .withSaleId(2)
+                        .withSalesmanName("Lucas")
+                        .withNewItem(
+                                ItemBuilder.builder()
+                                        .withItemId(1)
+                                        .withItemQuantity(2)
+                                        .withItemPrice(10)
+                                        .build()
+                        ).build());
+
+        mockSaleList.add(
+                SaleBuilder.builder()
+                        .withSaleId(3)
+                        .withSalesmanName("Lucas")
+                        .withNewItem(
+                                ItemBuilder.builder()
+                                        .withItemId(1)
+                                        .withItemQuantity(3)
+                                        .withItemPrice(10)
+                                        .build()
+                        ).build());
+
+        DataFile testDataFile = new DataFile("test",mockSalesmanList,null,mockSaleList);
+
+        String actual = testDataFile.worstSalesmanEver();
+
+        Assert.assertEquals("Marco",actual);
     }
 
 }
